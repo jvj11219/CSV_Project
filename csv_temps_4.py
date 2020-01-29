1c35d8a66b380727a9669d1e122f0e278f2e9f43
jvj11219 <joshj@entouch.net>
2020-01-29 14:49:32 -0600

    Fix csv_temps_4.py

--- csv_temps_4.py
+++ csv_temps_4.py
@@ -20,10 +20,16 @@ highs = []
 dates = []
 lows = []
 for row in csv_file:
-    highs.append(int(row[5]))
-    current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    dates.append(current_date)
-    lows.append(int(row[6]))
+    try:
+        high = int(row[4])
+        low = int(row[5])
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        highs.append(high)
+        highs.append(low)
+        dates.append(current_date)
 
 #Print 0-9 (top 10) from highs
 print(highs[:10])
