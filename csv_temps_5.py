b15084664324ed9c0c42590b8190b4be58c67bc4
jvj11219 <joshj@entouch.net>
2020-01-31 01:31:37 -0600

    Clean Up Formating and Variable Naming

--- csv_temps_5.py
+++ csv_temps_5.py
@@ -1,89 +1,90 @@
+# Josh Jacobsen - MW 2:30pm
+# CSV_Project
+
 import matplotlib.pyplot as plt
 import csv
 from datetime import datetime
 
-open_file_2 = open("death_valley_2018_simple.csv", "r")
-open_file = open("sitka_weather_2018_simple.csv", "r")
+#Load in File
+death_valley_file = open("death_valley_2018_simple.csv", "r")
+sitka_weather_file = open("sitka_weather_2018_simple.csv", "r")
 
-csv_file = csv.reader(open_file, delimiter=",")
-csv_file_2 = csv.reader(open_file_2, delimiter=",")
+#Read Files
+sitka_weather_csv = csv.reader(sitka_weather_file, delimiter=",")
+death_valley_csv = csv.reader(death_valley_file, delimiter=",")
 
-header_row = next(csv_file)
-header_row_2 = next(csv_file_2)
+#Get Headers
+sitka_header_row = next(sitka_weather_csv)
+death_valley_header_row = next(death_valley_csv)
 
-#Shows that the header row is a list
-print(type(header_row))
-print(type(header_row_2))
+#Display Data Type of the Header Row
+#print(type(sitka_header_row))
+#print(type(death_valley_header_row))
 
-#Loop through columns and print name and index
-for index,column_header in enumerate(header_row):
-    print(index,column_header)
+#Loop Through Columns and Print Column Names and Indexes
+#for index,column_header in enumerate(sitka_header_row):
+#    print(index,column_header)
+#for index,column_header in enumerate(death_valley_header_row):
+#    print(index,column_header)
 
-for index,column_header in enumerate(header_row_2):
-    print(index,column_header)
-
-#Create list and append high termperatures and dates
-highs_1 = []
-dates_1 = []
-lows_1 = []
-for row in csv_file:
+#Create list and Append High/Low Temps for Sitka
+sitka_highs = []
+sitka_dates = []
+sitka_lows = []
+for row in sitka_weather_csv:
     try:
-        high_1 = int(row[5])
-        low_1 = int(row[6])
+        sitka_high = int(row[5])
+        sitka_low = int(row[6])
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
     except ValueError:
         print(f'Missing data for {current_date}')
     else:
-        highs_1.append(high_1)
-        lows_1.append(low_1)
-        dates_1.append(current_date)
+        sitka_highs.append(sitka_high)
+        sitka_lows.append(sitka_low)
+        sitka_dates.append(current_date)
 
-#Create list and append high termperatures and dates
-highs_2 = []
-dates_2 = []
-lows_2  = []
-for row in csv_file_2:
+#Create list and Append High/Low Temps for Death Valley
+death_valley_highs = []
+death_valley_dates = []
+death_valley_lows  = []
+for row in death_valley_csv:
     try:
-        high_2 = int(row[4])
-        low_2 = int(row[5])
+        death_valley_high = int(row[4])
+        death_valley_low = int(row[5])
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
     except ValueError:
         print(f'Missing data for {current_date}')
     else:
-        highs_2.append(high_2)
-        lows_2.append(low_2)
-        dates_2.append(current_date)
+        death_valley_highs.append(death_valley_high)
+        death_valley_lows.append(death_valley_low)
+        death_valley_dates.append(current_date)
 
-#Print 0-9 (top 10) from highs
-print(highs_1[:10])
-print(dates_1[:10])
-print(highs_2[:10])
-print(dates_2[:10])
+#Print 0-9 (Top 10) Values from Highs
+#print(sitka_highs[:10])
+#print(sitka_dates[:10])
+#print(death_valley_highs[:10])
+#print(death_valley_dates[:10])
 
-#fig = plt.figure()
+#Create Subplots and Set Title
 fig, (ax1, ax2) = plt.subplots(2)
 fig.suptitle("Temperature comparison between SITKA AIRPORT, AK US and DEATH VALLEY, VA US",fontsize=10)
-ax1.plot(dates_1, highs_1, color="red", alpha=0.5)
-ax1.plot(dates_1, lows_1, color="blue", alpha=0.5)
-ax2.plot(dates_2, highs_2, color="red", alpha=0.5)
-ax2.plot(dates_2, lows_2, color="blue", alpha=0.5)
-ax1.fill_between(dates_1, highs_1, lows_1, facecolor='blue',alpha=0.1)
-ax2.fill_between(dates_2, highs_2, lows_2, facecolor='blue',alpha=0.1)
-ax1.set_title('SITKA AIRPORT, AK US',fontsize=8)
-ax2.set_title('DEATH VALLEY, CA US',fontsize=8)
-#ax2.xlabel("",fontsize=10)
 
+#Format Sitka Subplot
+ax1.plot(sitka_dates, sitka_highs, color="red", alpha=0.5)
+ax1.plot(sitka_dates, sitka_lows, color="blue", alpha=0.5)
+ax1.fill_between(sitka_dates, sitka_highs, sitka_lows, facecolor='blue',alpha=0.1)
+ax1.set_title('SITKA AIRPORT, AK US',fontsize=8)
 ax1.tick_params(axis="both",which="major",labelsize=8)
+
+#Format Death Valley Subplot
+ax2.plot(death_valley_dates, death_valley_highs, color="red", alpha=0.5)
+ax2.plot(death_valley_dates, death_valley_lows, color="blue", alpha=0.5)
+ax2.fill_between(death_valley_dates, death_valley_highs, death_valley_lows, facecolor='blue',alpha=0.1)
+ax2.set_title('DEATH VALLEY, CA US',fontsize=8)
 ax2.tick_params(axis="both",which="major",labelsize=8)
 
-#Formating
-#plt.plot(dates, highs, color="red", alpha=0.5)
-#plt.plot(dates, lows, color="blue", alpha=0.5)
-#plt.fill_between(dates, highs, lows, facecolor='blue',alpha=0.1)
-#plt.xlabel("",fontsize=10)
-#plt.ylabel("Temperature (F)", fontsize=12)
-#plt.tick_params(axis="both",which="major",labelsize=12)
-
+#Format Dates
 fig.autofmt_xdate()
 
+#Display Plots
 plt.show()
